f588a10f6c817cf3199b7b31f247337b0464635c
Andrew Buchilin <adrinaline@inbox.ru>
2023-09-12 15:33:41 +0300

    second try

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
+const isEmail = require('validator/lib/isEmail');
+const isUrl = require('validator/lib/isUrl');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -18,7 +19,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
-      validator: (v) => validator.isUrl(v),
+      validator: (v) => isUrl(v),
       message: 'Некорректный URL',
     },
   },
@@ -27,7 +28,7 @@ const userSchema = new mongoose.Schema({
     required: [true, 'Поле "email" должно быть заполнено'],
     unique: true,
     validate: {
-      validator: (v) => validator.isEmail(v),
+      validator: (v) => isEmail(v),
       message: 'Некорректный email',
     },
   },
